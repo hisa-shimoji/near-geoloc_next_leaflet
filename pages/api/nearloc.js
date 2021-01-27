b851adbc7aee6b7948ac588e53490083e1300d7c
Hisashi Shimoji <hshimoji1@mac.com>
2021-01-27 18:53:58 +0900

    mongodb connect

--- pages/api/nearloc.js
+++ pages/api/nearloc.js
@@ -1,6 +1,18 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { connectToDatabase } from "../../util/mongodb";
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+export default async (req, res) => {
+  const { db } = await connectToDatabase();
+  const neargeo = await db
+    .collection("stations")
+    .find({
+      coordinates: {
+        $near: { $geometry: { type: "Point", coordinates: [req.body.lng, req.body.lat] } },
+      },
+    })
+    .limit(req.body.points_num)
+    .toArray();
+
+  res.statusCode = 200;
+  res.json(neargeo);
+  // res.json(JSON.parse(JSON.stringify(neargeo.data)));
+};
